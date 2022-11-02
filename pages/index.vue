bf7ceefc1fd4cebbb6d56edf42c2a9c7c4860b2e
Peter Bessenyei <peter.bessenyei@gmail.com>
2022-11-02 08:04:00 +0100

    init

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div>
+      <nuxt-img
+        placeholder
+        format="webp"
+        sizes="sm:100vw md:50vw lg:800px"
+        src="/ivan-torres-MQUqbmszGGM-unsplash.jpg"
+        alt=""
+      />
+    </div>
+    <div>
+      <a class="info" href="https://image.nuxtjs.org/" target="_blank">
+        @nuxt/image homepage & docs
+      </a>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default {
+  data: () => ({
+    message: "Hello World!",
+  }),
+};
+</script>
